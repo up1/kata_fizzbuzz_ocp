d5a4dc17fc46634a8bb26e4ea287bcad0e481be4
somkiat <somkiat.p@gmail.com>
2014-06-06 15:04:33 +0700

    Add test second test case

--- src/test/java/kata/ocp/FizzBuzzTest.java
+++ src/test/java/kata/ocp/FizzBuzzTest.java
@@ -12,5 +12,12 @@ public class FizzBuzzTest {
 		assertEquals("1", fizzBuzz.say(1));
 		assertEquals("2", fizzBuzz.say(2));
 	}
+	
+	@Test
+	public void shouldReturnFizzForMultiplyOfThree() {
+		FizzBuzz fizzBuzz = FizzBuzzFactory.create();
+		assertEquals("Fizz", fizzBuzz.say(3));
+		assertEquals("Fizz", fizzBuzz.say(6));
+	}
 
 }
