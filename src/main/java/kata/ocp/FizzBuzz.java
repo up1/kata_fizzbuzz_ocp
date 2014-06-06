dc2652e0501dbf34c1829c66739f7c658e274839
somkiat <somkiat.p@gmail.com>
2014-06-06 15:09:36 +0700

    Add FizzRule to solve secon test case

--- src/main/java/kata/ocp/FizzBuzz.java
+++ src/main/java/kata/ocp/FizzBuzz.java
@@ -3,6 +3,10 @@ package kata.ocp;
 public class FizzBuzz {
 
 	public String say(Integer number) {
+		FizzRule fizzRule = new FizzRule();
+		if (fizzRule.isHandle(number)) {
+			return fizzRule.say();
+		}
 		return number.toString();
 	}
 
---
+++ src/main/java/kata/ocp/FizzRule.java
@@ -0,0 +1,13 @@
+package kata.ocp;
+
+public class FizzRule {
+
+	public boolean isHandle(Integer number) {
+		return number % 3 == 0;
+	}
+
+	public String say() {
+		return "Fizz";
+	}
+
+}
